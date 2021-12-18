d8a6b78eb1eaa40c201e992020216fc38c133f6a
vicuartas230 <vicuartas230@gmail.com>
2021-12-18 12:20:29 -0500

    Fix out of range issue

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,10 +4,13 @@
 
 def canUnlockAll(boxes):
     """ This method determines if all the boxes can be opened. """
-    keys = [key for key in boxes[0]]
+    keys = [key for key in boxes[0] if key < len(boxes)]
+    keys = list(set(keys))
     for i in keys:
+        if i > len(boxes) - 1:
+            continue
         for n in boxes[i]:
-            if n not in keys and n != 0:
+            if n not in keys and n != 0 and n < len(boxes):
                 keys.append(n)
     if len(keys) == len(boxes) - 1:
         return True
