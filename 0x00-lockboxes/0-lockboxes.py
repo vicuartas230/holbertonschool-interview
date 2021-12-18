9f166e792b2574afbc24ee4a59033653e7f12e43
vicuartas230 <vicuartas230@gmail.com>
2021-12-18 13:16:00 -0500

    Remove duplicated keys in final list without to sort

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,14 +4,15 @@
 
 def canUnlockAll(boxes):
     """ This method determines if all the boxes can be opened. """
+    no_repeated = []
     keys = [key for key in boxes[0] if key < len(boxes)]
-    keys = list(set(keys))
-    for i in keys:
+    [no_repeated.append(key) for key in keys if key not in no_repeated]
+    for i in no_repeated:
         if i > len(boxes) - 1:
             continue
         for n in boxes[i]:
-            if n not in keys and n != 0 and n < len(boxes):
-                keys.append(n)
-    if len(keys) == len(boxes) - 1:
+            if n not in no_repeated and n != 0 and n < len(boxes):
+                no_repeated.append(n)
+    if len(no_repeated) == len(boxes) - 1:
         return True
     return False
