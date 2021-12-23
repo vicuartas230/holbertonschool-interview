ba8a4f330eb01c8f8b5d8eff218042c59523994b
vicuartas230 <vicuartas230@gmail.com>
2021-12-22 19:55:27 -0500

    Remove comments

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -18,9 +18,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     new->parent = parent;
     new->left = NULL;
     new->right = NULL;
-    /* if (!parent)
-        parent = new;
-    else
-        parent->left = new; */
     return (new);
 }
