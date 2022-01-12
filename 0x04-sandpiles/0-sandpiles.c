abf84e51d9f85e639a924bb1b2a46a4d0c35400e
vicuartas230 <vicuartas230@gmail.com>
2022-01-11 20:00:43 -0500

    Upload readme file

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,11 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - This function computes the sum of two sandpiles.
+ * @grid1: First array of numbers.
+ * @grid2: Second array of numbers.
+ * Return: Nothing.
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{}
