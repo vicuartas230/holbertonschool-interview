77c19618169a0c0a9a67d8cf952a87307dcd7111
vicuartas230 <vicuartas230@gmail.com>
2022-01-13 19:04:49 -0500

    Initial code

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -8,4 +8,36 @@
  */
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
-{}
+{
+    int i = 0, j = 0, total[3][3];
+
+    while (i < 3)
+    {
+        while (j < 3)
+        {
+            total[i][j] = grid1[i][j] + grid2[i][j];
+            j++;
+        }
+        i++;
+    }
+}
+
+int review_sandpile(int grid[3][3])
+{
+    int i = 0, j = 0;
+
+    while (i < 3)
+    {
+        while (j < 3)
+        {
+            if (grid[i][j] > 3)
+            {
+                return (0);
+            }
+            
+            j++;
+        }
+        i++;
+    }
+    return (1);
+}
