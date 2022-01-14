bc9026587a62e25ea40acf5362fb1b1b317ec3b6
vicuartas230 <vicuartas230@gmail.com>
2022-01-13 22:38:07 -0500

    First test

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -9,80 +9,92 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i = 0, j = 0, total[3][3];
+	int i = 0, j = 0;
 
-    while (i < 3)
-    {
-        j = 0;
-        while (j < 3)
-        {
-            grid1[i][j] = grid1[i][j] + grid2[i][j];
-            j++;
-        }
-        i++;
-    }
-    round_sandpile(grid1);
+	while (i < 3)
+	{
+		j = 0;
+		while (j < 3)
+		{
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+			j++;
+		}
+		i++;
+	}
+	round_sandpile(grid1);
 }
+
+/**
+ * review_sandpile - This function reviews if a grid is stable.
+ * @grid: The matrix of numbers.
+ * Return: 0 if grid is stable and 1 if not. 
+ */
 
 int review_sandpile(int grid[3][3])
 {
-    int i = 0, j = 0;
+	int i = 0, j = 0;
 
-    while (i < 3)
-    {
-        j = 0;
-        while (j < 3)
-        {
-            if (grid[i][j] > 3)
-                return (1);
-            j++;
-        }
-        i++;
-    }
-    return (0);
+	while (i < 3)
+	{
+		j = 0;
+		while (j < 3)
+		{
+			if (grid[i][j] > 3)
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
+
+/**
+ * round_sandpile - This function stabilizes a grid.
+ * @grid: The matrix of numbers.
+ * Return: Nothing.
+ */
 
 void round_sandpile(int grid[3][3])
 {
-    int i = 0, j = 0;
+	int i = 0, j = 0;
 
-    if (review_sandpile(grid))
-    {
-        while (i < 3)
-        {
-            j = 0;
-            while (j < 3)
-            {
-                if (grid[i][j] >= 4)
-                {
-                    grid[i][j] -= 4;
-                    grid[i - 1][j]++;
-                    grid[i][j - 1]++;
-                    grid[i][j + 1]++;
-                    grid[i + 1][j]++;
-                }
-                j++;
-            }
-            i++;
-        }
-        print_grid(grid);
-        printf("\n");
-        round_sandpile(grid);
-    }
+	if (review_sandpile(grid))
+	{
+		while (i < 3)
+		{
+			j = 0;
+			while (j < 3)
+			{
+				if (grid[i][j] >= 4)
+				{
+					grid[i][j] -= 4;
+					grid[i - 1][j]++;
+					grid[i][j - 1]++;
+					grid[i][j + 1]++;
+					grid[i + 1][j]++;
+				}
+				j++;
+			}
+			i++;
+		}
+		print_grid(grid);
+		printf("\n");
+		round_sandpile(grid);
+	}
 }
 
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,5 +5,8 @@
 #include <stdlib.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int review_sandpile(int grid[3][3]);
+void round_sandpile(int grid[3][3]);
+static void print_grid(int grid[3][3]);
 
 #endif
