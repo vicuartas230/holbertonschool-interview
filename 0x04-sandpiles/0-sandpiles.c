5f60c3a8d9ae83227d6476127f2ed284f4b9d599
vicuartas230 <vicuartas230@gmail.com>
2022-01-15 22:35:36 -0500

    Change recursion by loop

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -21,6 +21,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		}
 		i++;
 	}
+	printf("\n");
 	print_grid(grid1);
 	printf("\n");
 	round_sandpile(grid1);
@@ -59,20 +60,17 @@ int review_sandpile(int grid[3][3])
 void round_sandpile(int grid[3][3])
 {
 	int i = 0, j = 0,
-	total[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	aux[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
-	if (review_sandpile(grid))
+	while (review_sandpile(grid))
 	{
-		while (i < 3)
+		for (i = 0; i < 3; i++)
 		{
-			j = 0;
-			while (j < 3)
+			for (j = 0; j < 3; j++)
 			{
 				if (grid[i][j] < 4)
-					total[i][j] = grid[i][j];
-				j++;
+					aux[i][j] = grid[i][j];
 			}
-			i++;
 		}
 		for (i = 0; i < 3; i++)
 		{
@@ -80,22 +78,16 @@ void round_sandpile(int grid[3][3])
 			{
 				if (grid[i][j] >= 4)
 				{
-					total[i][j] += grid[i][j] - 4;
-					total[i + 1][j] += 1;
-					total[i - 1][j] += 1;
-					total[i][j + 1] += 1;
-					total[i][j - 1] += 1;
+					aux[i][j] += grid[i][j] - 4;
+					aux[i + 1][j]++;
+					aux[i - 1][j]++;
+					aux[i][j + 1]++;
+					aux[i][j - 1]++;
 				}
 			}
 		}
-		printf("grid:\n");
+		reset_grid(grid, aux);
 		print_grid(grid);
-		printf("\n");
-		grid = total;
-		printf("total:\n");
-		print_grid(total);
-		printf("\n");
-		round_sandpile(grid);
 	}
 }
 
@@ -118,5 +110,25 @@ static void print_grid(int grid[3][3])
 			printf("%d", grid[i][j]);
 		}
 		printf("\n");
+	}
+}
+
+/**
+ * reset_grid - This function initializes a grid.
+ * @grid: The array to set to 0.
+ * Return: Nothing.
+ */
+
+void reset_grid(int grid[3][3], int aux[3][3])
+{
+	int i = 0, j = 0;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid[i][j] = aux[i][j];
+			aux[i][j] = 0;
+		}
 	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -8,5 +8,6 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 int review_sandpile(int grid[3][3]);
 void round_sandpile(int grid[3][3]);
 static void print_grid(int grid[3][3]);
+void reset_grid(int grid[3][3], int aux[3][3]);
 
 #endif
